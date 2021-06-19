828b39556cedeca44a565a6750bfb268b6bb3d85
jamsheed6767 <85213624+jamsheed6767@users.noreply.github.com>
2021-06-19 11:31:31 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,62 +1,84 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
-int max (int arr[], int n)  {
-
-  for (int i=0;i<n-1;i++)  {
-    int swap = 0;
-    for (int j=0;j<n-i-1;j++) {
-      if (arr[j]>arr[j+1])  {
-        int t = arr[j+1];
-        arr[j+1] = arr[j];
-        arr[j] = t;
-        swap=1;
-      }
-    }
-    if (swap==0)
-      break;
-  }
-  int max = arr[n-1];  
-  return max;
-}
-
-int min (int arr[], int n)  {
-
-  for (int i=0;i<n-1;i++)  {
-    int swap = 0;
-    for (int j=0;j<n-i-1;j++) {
-      if (arr[j]>arr[j+1])  {
-        int t = arr[j+1];
-        arr[j+1] = arr[j];
-        arr[j] = t;
-        swap=1;
-      }
-    }
-    if (swap==0)
-      break;
-  }
-  int min = arr[0];
-  return min;
-}
-
-float average (int arr[], int n)  {
-
-  for (int i=0;i<n-1;i++)  {
-    int swap = 0;
-    for (int j=0;j<n-i-1;j++) {
-      if (arr[j]>arr[j+1])  {
-        int t = arr[j+1];
-        arr[j+1] = arr[j];
-        arr[j] = t;
-        swap=1;
-      }
-    }
-    if (swap==0)
-      break;
-  }
-  int sum = 0;
+float average(int z[], int n)
+{
+ int sum =0;
   for (int i=0;i<n;i++)
-    sum=sum+arr[i];
-  float avg = (float)sum/n;
-  
+  {
+    sum +=z[i];
+  }
+ float avg;
+  avg = (float)sum/n;
   return avg;
+}
+int factors(int n, int r[])
+{
+  int p[] ={2,3,5,7,9,11,13,17,19};
+  int sum =0 , j=0;
+ for(int i=0;i<9;i++)
+ {
+     while(n%p[i] ==0)
+  { r[j]=p[i];
+  n=n/p[i];
+   sum +=1;
+   j++;
+      
+  }
+     
+ }
+  return sum;
+}
+int max(int z[], int n)
+{
+  for(int i=0;i<=n;i++)
+  {
+    int sum=0;
+    for(int j=0;j<=n;j++)
+     {
+       if(z[i]>=z[j])
+       {
+         sum +=1;
+       }
+      if(sum == n)
+        return z[i];
+    }
+  }
+}
+ int min(int z[], int n)
+{
+  for(int i=0;i<=n;i++)
+  {
+    int sum=0;
+    for(int j=0;j<n;j++)
+     {
+       if(z[i]<=z[j])
+       {
+         sum +=1;
+       }
+      if(sum == n)
+        return z[i];
+    }
+  }
+}
+int mode(int z[], int n)
+{
+  int count[n], sum =0;
+  for(int i=0;i<n;i++)
+  {
+    for(int j=0;j<n;j++)
+    {
+      if(z[i]==z[j])
+        count[i] +=1;
+    }
+  }
+   for(int i=0;i<n;i++)
+  {
+    for(int j=0;j<n;j++)
+    {
+      if(count[i]>=count[j])
+        sum +=1;
+      if(sum==n){
+        return z[i];}
+    }
+   }
 }
